83d9f7dce8bc545dd6275dd35e865643881f6b67
sujal-shrestha <sujalwork69@gmail.com>
2025-05-19 14:29:59 +0545

    Add splash screen with 2-second delay

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const LoginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text(
+          'Adora',
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.blue),
+        ),
+      ),
+    );
+  }
+}
