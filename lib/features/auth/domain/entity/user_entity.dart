187adcb03c700e6db8aa781f7ee83dfe3aa79e6a
sujal-shrestha <sujalwork69@gmail.com>
2025-07-03 22:44:28 +0545

    feat: add password and token to UserEntity with toJson/fromJson

--- lib/features/auth/domain/entity/user_entity.dart
+++ lib/features/auth/domain/entity/user_entity.dart
@@ -2,10 +2,32 @@ class UserEntity {
   final String name;
   final String email;
   final String password;
+  final String? token;
 
-  UserEntity({
+  const UserEntity({
     required this.name,
     required this.email,
     required this.password,
+    this.token,
   });
+
+  // Factory constructor used when receiving user data from API (usually no password in response)
+  factory UserEntity.fromJson(Map<String, dynamic> json) {
+    return UserEntity(
+      name: json['name'] ?? '',
+      email: json['email'] ?? '',
+      password: '', // password not returned from server
+      token: json['token'],
+    );
+  }
+
+  // Used when sending data to API
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'email': email,
+      'password': password,
+      if (token != null) 'token': token,
+    };
+  }
 }
