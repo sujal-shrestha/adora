a2e0dd90c4d381517af4dcf208a139b870108d98
sujal-shrestha <sujalwork69@gmail.com>
2025-08-03 17:12:44 +0545

    chore(auth): remove deprecated local repository implementations

--- lib/features/auth/data/repository/user_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:adora_mobile_app/features/auth/domain/entity/user_entity.dart';
-
-abstract class UserRepository {
-  Future<void> registerUser(UserEntity user);
-  Future<UserEntity?> loginUser(String email, String password);
-}
